84f5abb10a8ee1b74952a560655bebde17383d7d
Olivier Castanos <olivier.castanos@ITS4U225m.local>
2023-10-24 15:57:24 +0200

    update

--- src/main/java/lu/its4u/consumer/kafka/KafkaConsumer.java
+++ src/main/java/lu/its4u/consumer/kafka/KafkaConsumer.java
@@ -5,7 +5,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import lu.its4u.dto.Data;
-import lu.its4u.dto.Event;
 
 @Component
 public class KafkaConsumer {
@@ -13,8 +12,9 @@ public class KafkaConsumer {
 	public void consume(ConsumerRecord<String, Data> data) {
 		Data e = data.value();
 
-		System.out.println("recu:\n -- Id: " + e.getId()+"--"+e.getEnvironment() + "\n -- Description: " + e.getDescription() + "\n -- Name:"
-				+ e.getName() + "\n -- Date: " + e.getDate() + "\n -_______________________-");
+		System.out.println("recu:\n -- Id: " + e.getId() + "--" + "\n -- Description: " + e.getDescription()
+				+ "\n -- Environment: " + e.getEnvironment() + "\n -- Name:" + e.getName() + "\n -- Date: "
+				+ e.getDate() + "\n -_______________________-");
 
 	}
 
